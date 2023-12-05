f7043b0f87c6c937ed48199fa640caf062e128d2
alvin19980205 <79758511+alvin19980205@users.noreply.github.com>
2023-12-04 23:32:35 -0500

    Add files via upload

---
+++ Main/Group_Page.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link href="Group_css.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+<header class="h1">
+      <div class="left-side">
+        <img class="banner3" src="https://image.flaticon.com/icons/png/512/123/123392.png" style= "width:250px">
+        <hr class="divider">
+        <div class="page-title">
+          <p class="title-main">Student Scheduler</p>
+          <p class="title">Redefining student scheduling</p>
+        </div>
+      </div>
+      <img class="banner1" src="https://image.flaticon.com/icons/png/512/60/60785.png" style= "width:100px">
+    </header>
+	<h1>Meet the Group!</h1>
+	
+	<!-- using an image with URL gotten from https://www.w3schools.com/html/html_images.asp-->
+	
+	<p><img src="" width="1000" height="400"alt="group-pic" border="0"></p>
+	
+	
+<p><a href="Main_Page.php" class = "button1">Main Page</a></p>
+</body>
+</html>
