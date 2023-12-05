51ec34ab6a404827fcb0c178f4757fc76006cfeb
alvin19980205 <79758511+alvin19980205@users.noreply.github.com>
2023-12-04 23:30:49 -0500

    Update Main_Page.php

--- Main_Page.php
+++ Main_Page.php
@@ -1,36 +1,36 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <title>Student Scheduler</title>
-    <link rel="icon" type="image/ico" href="https://web.ics.purdue.edu/~g1117490/main/ie.ico"/>
+    <title>Car Rental</title>
+    <link rel="icon" type="image/ico" href="https://img.freepik.com/free-vector/car-rental-concept-illustration_114360-9267.jpg/>
     <link href="Main_css.css" rel="stylesheet" type="text/css" />
   </head>
   <body>
     <header class="h1">
       <div class="left-side">
-        <img class="banner3" src="https://www.flaticon.com/svg/vstatic/svg/4185/4185688.svg?token=exp=1618089495~hmac=84e1937dad55bad2ee74e52fe392a686" style= "width:250px">
+        <img class="banner3" src="https://img.freepik.com/free-vector/car-rental-concept-illustration_114360-9267.jpg" style= "width:250px">
         <hr class="divider">
         <div class="page-title">
-          <p class="title-main">Student Scheduler</p>
-          <p class="title">Redefining student scheduling</p>
+          <p class="title-main">Car Rental</p>
+          <p class="title">Car Rental Website</p>
         </div>
       </div>
-      <img class="banner1" src="https://www.flaticon.com/svg/vstatic/svg/4185/4185823.svg?token=exp=1618088399~hmac=85714128e97e4f96030e0e83ec09bc99" style= "width:100px">
+      <img class="banner1" src="https://img.freepik.com/free-vector/car-rental-concept-illustration_114360-9267.jpg" style= "width:100px">
     </header>
     <div class="page">
-      <img class="hand" src="https://www.flaticon.com/svg/vstatic/svg/4185/4185301.svg?token=exp=1618093386~hmac=a726bca543c696bb3e2496c2427e926d">
+      <img class="hand" src="https://img.freepik.com/free-vector/car-rental-concept-illustration_114360-9267.jpg">
       <div class="person-options">
         <div class="button">
-          <a href = "student/student_login.php" class="button" target="_self" style= "padding:0">Student Login</a>
+          <a href = "user/user_login.php" class="button" target="_self" style= "padding:0">User Login</a>
         </div>
         <div class="button">
-          <a href="employer/advisor_login.php" class="button" target="_self" style= "padding:0">Advisor Login</a>
+          <a href="employee/employee_login.php" class="button" target="_self" style= "padding:0">Employee Login</a>
         </div>
-        <h2><p><a href="Group_Page.php" class = "button1">IE 332 Team 9 Group</a></p></h2>
+        <h2><p><a href="Group_Page.php" class = "button1">CSGY-6083 Group</a></p></h2>
       </div>
     </div>
   <footer class="footer">
-    NON-AFFILIATION DISCLAIMER: This website is not endorsed by, directly affiliated with, maintained, authorized, or sponsored by Purdue University.
+    NON-AFFILIATION DISCLAIMER: This website is not endorsed by, directly affiliated with, maintained, authorized, or sponsored by New York University.
   </footer>
   </body>
 </html>
